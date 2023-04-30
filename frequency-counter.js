3bb46007b654ead54d022d3843ddabdb823f2e48
Abdallah Alathamneh <60315219+Abdallah991@users.noreply.github.com>
2023-04-30 17:40:29 +0300

    solved and refined

--- frequency-counter.js
+++ frequency-counter.js
@@ -2,7 +2,7 @@
 // an angram returns true if the two strings have the same letters with the same frequency.
 // it retrns false otherwise.
 // helpful controls, length of words
-// frequency of letters and letters un an object
+// frequency of letters and letters in an object
 // through for loops, key as string and frequency as the value.
 // compare them in the final loop
 
@@ -11,17 +11,15 @@ anagram = (word1, word2) => {
   if (word1.length != word2.length) return false;
   //   define objects to find the frequency of letters
   var object1 = {};
-  var object2 = [];
-  //   find the frequency for word 1
+  var object2 = {};
   for (i = 0; i < word1.length; i++) {
+    // frequency of word 1
     if (!object1[word1[i]]) {
       object1[word1[i]] = 1;
     } else {
       object1[word1[i]]++;
     }
-  }
-  //   find the frequency of word 2
-  for (i = 0; i < word2.length; i++) {
+    //   find the frequency of word 2
     if (!object2[word2[i]]) {
       object2[word2[i]] = 1;
     } else {
@@ -29,24 +27,23 @@ anagram = (word1, word2) => {
     }
   }
 
+  // ? to see your objects
   console.log(object1);
   console.log(object2);
 
-  //   loop over the letters and make sure they have the same frequency, if not return false
+  //  loop over the letters and make sure they have the same frequency, if not return false
   // if all correct return true
-
   for (i = 0; i < word1.length; i++) {
-    if (object1[word1[i]] == object2[word1[i]]) {
-      // return false;
-      // console.log(object1[word1[i]] == object2[word1[i]]);
-    } else {
+    if (object1[word1[i]] != object2[word1[i]]) {
       return false;
-      // console.log(object1[word1[i]] == object2[word1[i]]);
     }
   }
-
   return true;
-
-  // return true;
 };
+
+// the time complexity is O(n)
+// two loops, no nested loops
+// space complexity O(n)
+// two objects, two strings
+// ? call the function
 console.log(anagram("abdallah", "abdallah"));
