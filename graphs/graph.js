d3014a4b5af30b357096972e1662c5838ba2c8f6
abdallah991 <alathamneh.abdallah@gmail.com>
2024-01-28 14:12:02 +0300

    graph traversal

--- graphs/graph.js
+++ graphs/graph.js
@@ -12,6 +12,7 @@ class Graph {
     // validate input
     if (!this.adjacencyList[vertex1]) return;
     if (!this.adjacencyList[vertex2]) return;
+    // declare found boleans
     let found1 = false;
     let found2 = false;
     // check all edges in vertex1
@@ -56,15 +57,75 @@ class Graph {
     // remove key from adjacency list
     delete this.adjacencyList[vertex];
   }
+
+  depthFirst(start) {
+    // to store the vertices visited
+    let result = [];
+    // to store the if vertices were visited
+    let visited = {};
+    const adjacencyList = this.adjacencyList;
+    // recursive helper function
+    function search(vertex) {
+      // if the vertex is empty
+      if (!vertex) return;
+      // mark the vertex as visited
+      visited[vertex] = true;
+      // push the vertex to results
+      result.push(vertex);
+      // loop over each edge for that vertex
+      adjacencyList[vertex].forEach((connectedTo) => {
+        // if connectedTo does not, invoke function
+        if (!visited[connectedTo]) {
+          search(connectedTo);
+        }
+      });
+    }
+    // invoke search
+    search(start);
+    // return result
+    return result;
+  }
+
+  breadthFirst(vertex) {
+    // declare queue and visited and their initial values
+    let queue = [vertex];
+    let visited = {};
+    visited[vertex] = true;
+    let result = [];
+    let currentVertex;
+
+    // queue is not empty
+    while (queue.length) {
+      // remove element form queue and add to result
+      currentVertex = queue.shift();
+      result.push(currentVertex);
+      // loop over edges
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        // if not visited
+        if (!visited[neighbor]) {
+          // mark as visited
+          visited[neighbor] = true;
+          // add to queue
+          queue.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
 }
 
 let graph = new Graph();
-graph.addVertex("Tokyo");
-graph.addVertex("Manama");
-graph.addVertex("Doha");
-graph.addEdge("Manama", "Tokyo");
-graph.addEdge("Manama", "Doha");
-graph.addEdge("Tokyo", "Doha");
-graph.removeVertex("Tokyo");
-
-console.log(graph);
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+graph.addVertex("F");
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+graph.addEdge("B", "D");
+graph.addEdge("C", "E");
+graph.addEdge("D", "E");
+graph.addEdge("D", "F");
+graph.addEdge("E", "F");
+console.log(graph.breadthFirst("A"));
